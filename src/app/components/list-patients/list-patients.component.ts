bc2af6edfb34d133d5fd0e1116e5e659ae92b3b9
Jaffar MOUKAILA <jmoukaila@yahoo.fr>
2023-03-12 14:04:20 +0000

    Firebase RTDB in Consultation & patientList search

--- src/app/components/list-patients/list-patients.component.ts
+++ src/app/components/list-patients/list-patients.component.ts
@@ -1,12 +1,14 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MDBModalRef, MDBModalService, MdbTableDirective, MdbTablePaginationComponent} from 'angular-bootstrap-md';
 import {ModalEditComponent} from '../modal-edit/modal-edit.component';
 import {Store} from '@ngrx/store';
 import {deletePatient, loadPatients, upsertPatient} from '../../actions/patient.actions';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Patient} from '../../models/patient.model';
 import {ModalConfirmationComponent} from '../modal-confirmation/modal-confirmation.component';
 import {selectPatientsListFeatureState} from '../../reducers';
+import { selectAllPatients } from 'src/app/reducers/patient.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -20,12 +22,20 @@ export class ListPatientsComponent implements OnInit, OnDestroy, AfterViewInit {
 
   elements: Observable<Patient []>; // This will be used as observable of the arraylist of patients
   headElements = ['Id', 'Nom', 'Prénom', 'Tél. 1', 'Tél. 2', 'E-mail', 'Opération']; // Datatable header titles
+  nameSearch: string = '';
+  surnameSearch: string = '';
+  telSearch: string = '';
+  previous: string;
   modalRef: MDBModalRef;
 
   constructor(
     private cdRef: ChangeDetectorRef,
     private modalService: MDBModalService,
     private store: Store) {
+  }
+
+  @HostListener('input') oninput() {
+    this.searchItems();
   }
 
   ngOnInit(): void {
@@ -35,6 +45,7 @@ export class ListPatientsComponent implements OnInit, OnDestroy, AfterViewInit {
     this.elements = this.store.select(selectPatientsListFeatureState);
     // Feeding of the table with the data source
     this.mdbTable.setDataSource(this.elements);
+    this.previous = this.mdbTable.getDataSource();    
   }
 
   ngAfterViewInit(): void {
@@ -47,6 +58,7 @@ export class ListPatientsComponent implements OnInit, OnDestroy, AfterViewInit {
 
   editRow(el: Patient): void {
     const modalOptions = {
+      scroll: true,
       data: {
         editableRow: el
       }
@@ -66,6 +78,52 @@ export class ListPatientsComponent implements OnInit, OnDestroy, AfterViewInit {
         this.store.dispatch(deletePatient({patient: el}));
       }
     );
+  }
+
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+    if(this.nameSearch==''&&this.surnameSearch==''&&this.telSearch=='') {
+      this.mdbTable.setDataSource(this.previous);
+      this.elements = this.mdbTable.getDataSource();
+    }
+    else {
+      this.elements = this.elements.pipe(
+        map(data => data.filter(value => this.recherche(value, this.nameSearch, this.surnameSearch, this.telSearch)))
+      );
+     this.mdbTable.setDataSource(prev);
+    }
+  }
+
+  recherche(data: Patient, nom:string, prenom: string, tel:string): boolean {
+    if(nom.trim()==''){
+      if(prenom.trim()==''){
+        if(tel){
+          return (data.tel1.includes(tel)||data.tel2.includes(tel))
+        }
+      }
+      else {
+        if(tel){
+          return data.prenom.includes(prenom)&&(data.tel1.includes(tel)||data.tel2.includes(tel))
+        }
+        else {
+          return data.prenom.includes(prenom)
+        }
+      }
+    }
+    else {
+      if(prenom){
+        if(tel){
+          return data.nom.includes(nom)&&data.prenom.includes(prenom)&&
+          (data.tel1.includes(tel)||data.tel2.includes(tel))
+        }
+        else {
+          return data.nom.includes(nom)&&data.prenom.includes(prenom)
+        }
+      }
+      else {
+        return data.nom.includes(nom)
+      }
+    }    
   }
 
   ngOnDestroy(): void {
--- src/app/services/consultation.service.ts
+++ src/app/services/consultation.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Consultation} from '../models/consultation.model';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { map, take, tap } from 'rxjs/operators';
 
 const consultationUrl = 'http://localhost:8000/consultation';
 
@@ -10,22 +12,79 @@ const consultationUrl = 'http://localhost:8000/consultation';
 })
 export class ConsultationService {
 
-  constructor(private http: HttpClient) {
+  private consultationsWithKeys:Observable<any>;
+  private consultationsRef: AngularFireList<Consultation>;
+  private consultations: Observable<Consultation[]>;
+
+  constructor(
+    // Uncomment the below line if you want to use a local database like MySQL for e.g
+    // private http: HttpClient,
+    private db: AngularFireDatabase) {
+      this.consultationsRef = db.list<Consultation>('/consultation');
+      this.consultations = this.consultationsRef.valueChanges();
+      // Use snapshotChanges().map() to store the key
+      this.consultationsWithKeys = this.consultationsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(p => ({ key: p.payload.key, ...p.payload.val() }))
+      )
+    );
   }
 
-  // Method to create a new consultation
-  saveConsultation(consultation: Consultation): Observable<Consultation> {
-    return this.http.post<Consultation>(consultationUrl, consultation);
-  }
+/**
+   * Method to create a new consultation 
+   * @param consultation 
+   * @returns an Observable of the new consultation
+   */
+saveConsultation(consultation: Consultation): Observable<Consultation> {
+  // Uncomment the below line if you want to use a local database like MySQL for e.g and comment others
+  // return this.http.post<consultation>(consultationsUrl, consultation);
+  this.consultationsRef.push(consultation);
+  return this.consultations.pipe(map(values => values.pop()));
+}
 
-  // Method to retrieve all registered consultations from database
-  getConsultations(): Observable<Consultation[]> {
-    return this.http.get<Consultation[]>(consultationUrl);
-  }
+/**
+ * Method to retrieve all registered consultations from database
+ * @returns an Observable of all registered consultations
+ */
+getConsultations(): Observable<Consultation[]> {
+  // Uncomment the below line if you want to use a local database like MySQL for e.g and comment others
+  // return this.http.get<consultation[]>(consultationsUrl);
+  return this.consultations;
+}
 
-  // Method to update a consultation
-  updateConsultation(consultation: Consultation): Observable<Consultation> {
-    return this.http.put<Consultation>(consultationUrl, consultation);
-  }
+/**
+ * Method to update a consultation
+ * @param consultation 
+ * @returns an Observable of the updated consultation
+ */
+updateConsultation(consultation: Consultation): Observable<Consultation> {
+  // Uncomment the below line if you want to use a local database like MySQL for e.g and comment others
+  // return this.http.put<consultation>(consultationsUrl, consultation);
+  let key: string;
+  this.consultationsRef.snapshotChanges().pipe(
+    map(data => data.find(data => data.payload.val().codeCons == consultation.codeCons)),
+    take(1),
+    tap(value => this.consultationsRef.update(value.key, consultation))
+  ).subscribe();
+  return this.consultations.pipe(map(values => values.find(value => value.codeCons == consultation.codeCons)));
+}
+
+/**
+ * Method to delete a consultation
+ * @param consultation 
+ * @returns an Observable of the deleted consultation
+ */
+deleteConsultation(consultation: Consultation): Observable<Consultation> {
+  // Uncomment the below line if you want to use a local database like MySQL for e.g and comment others
+/*     return this.http.delete<consultation>(consultationsUrl,
+    {observe: 'body', responseType: 'json', body: consultation}); */
+    let key: string;
+    this.consultationsRef.snapshotChanges().pipe(
+      map(data => data.find(data => data.payload.val().codeCons == consultation.codeCons)),
+      take(1),
+      tap(value => this.consultationsRef.remove(value.key))
+    ).subscribe();
+  return this.consultations.pipe(map(values => values.find(value => value.codeCons == consultation.codeCons)));
+}
 
 }
